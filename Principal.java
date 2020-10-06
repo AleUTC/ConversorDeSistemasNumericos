c90ec9f4cb58962a83daa1537e7919a53f4f385b
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-10-05 20:19:53 -0500

    ajustes en clase principal para iniciar el programa

--- Principal.java
+++ Principal.java
@@ -1,32 +1,42 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Principal {
 
-    public static void main(String[] args) {
-        int opc, numero;
+    public static void main(String[] args){
+        int sistema, continuar;
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Bienvenido al programa para convertir un numero a cualquiera de los siguientes Sistemas Numericos: \n\t1) Binario \n\t2) Decimal \n\t3) Octal \n\t4) Hexadecimal");
-
-        do{
-            System.out.println("Ingresa un numero entero mayor que 0");
-            numero = sc.nextInt();
-        }while(numero < 0);
-
         do {
-            System.out.println("Selecciona el Sistema Numerico al que deseas hacer la conversion: \n\t1) Binario \n\t2) Decimal \n\t3) Octal \n\t4) Hexadecimal");
-            opc = sc.nextInt();
+            System.out.println("\n\nBienvenido al programa para convertir un numero a cualquiera de los siguientes Sistemas Numericos: Binario, Decimal, Octal y Hexadecimal");
+            System.out.println("\nSelecciona el Sistema Numerico del numero que vas a ingresar: \n\t1) Decimal \n\t2) Binario  \n\t3) Octal \n\t4) Hexadecimal");
+            sistema = sc.nextInt();
 
-            switch (opc) {
-                case 1://Binario
-                    Binarios obj1 = new  Binarios();
-                    obj1.convertirBinario(numero);
+            switch (sistema) {
+                case 1: //Menu Decimal
+                    Menu menu_10 = new Menu();
+                    menu_10.menuDecimal();
                     break;
+
+                case 2://Menu Binario
+                    Menu menu_2 = new Menu();
+                    menu_2.menuBinario();
+
+                case 3://Menu Octal
+                    Menu menu_3 = new Menu();
+                    menu_3.menuOctal();
+
+                case 4://Menu Hexadecimal
+                Menu menu_4 = new Menu();
+                menu_4.menuHexadecimal();
 
                 default:
+                    System.out.println("Opcion invalida, intenta de nuevo");
                     break;
             }
-        } while (opc ==  0);
+            System.out.println("\n\nDeseas realizar otra conversion entre Sistemas Numericos \n\nSi presiona (1) || No presiona (0)");
+            continuar = sc.nextInt();
+        } while (continuar !=  0);
+
     }
 }
