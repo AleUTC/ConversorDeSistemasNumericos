6f5ba5132bde98f530b565adf92cd872334b5bf3
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-10-05 20:17:56 -0500

    Modifique el metodo para decimal a binario

--- Binario.java
+++ Binario.java
@@ -1,6 +1,6 @@
 public class Binario {
 
-    public void decimalBinario(int numeroIni){
+    public double decimalBinario(int numeroIni){
         int numero = numeroIni, digito, exponente = 0;
         double  binario = 0;
 
@@ -11,21 +11,9 @@ public class Binario {
             numero = numero / 2;
         }
         //System.out.printf("La conversión del numero "+ numeroIni +"Decimal a Binario es: %.0f %n", binario);
-        System.out.printf("Binario: %.0f %n",binario);
+        //System.out.printf("Binario: %.0f %n",binario);
+        return binario;
    }
 
-   public void binarioDecimal(long numeroIni){
-       long digito, decimal = 0;
-       int exponente = 0;
 
-       digito = numeroIni % 10;
-
-        while (numeroIni != 0) {
-            digito = numeroIni % 10;
-            decimal = decimal + digito * (int) Math.pow(2, exponente);
-            exponente++;
-            numeroIni = numeroIni / 10;
-        }
-        System.out.println("Decimal: "+ decimal);
-   }
 }
